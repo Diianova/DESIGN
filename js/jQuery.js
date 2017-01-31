a299c3dbed5705764d1a177f2e103644bcc0a397
Diianova <olga.diianova@gmail.com>
2017-01-31 18:38:13 +0100

    desktop version_sticky menu

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,5 +1,19 @@
 $(function(){
 
+// Sticky menu
+var mainNav = $(".page-nav");
+var mns = "main-nav-scrolled";
+var	hdr = $('header').height();
+var container = $(".container");
+	
+$(window).scroll(function(){
+	if( $(this).scrollTop() > hdr){
+		mainNav.addClass(mns);
+	}else{
+		mainNav.removeClass(mns);
+	}
+});
+
 // Photo boxes
 var allPeople = $('section').find('#menu3 .foto');
 var fadeInP =  $('section').find('#menu3 .foto p');
